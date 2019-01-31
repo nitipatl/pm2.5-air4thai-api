f947f6144e0b5da91679c094b800f30f7d5c9bdc
Nitipat L <iamsvz@gmail.com>
2019-01-31 15:43:33 +0700

    [Edited] add route 'api'

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/api', (req, res) => {
+    res.send('test')
+  })
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
