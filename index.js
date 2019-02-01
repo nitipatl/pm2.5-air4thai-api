f11ee8329420db82ce4430340f04bede7134c6f7
Nitipat L <iamsvz@gmail.com>
2019-02-01 10:02:31 +0700

    [Edited] uncomment for debug

--- index.js
+++ index.js
@@ -16,57 +16,57 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  // .get('/history/:station', (req, res) => {
-  //   request(historyUrl + req.params.station, function (error, response, body) {
-  //     return res.status(200)
-  //     .type('text/html')
-  //     .send(body)
-  //   })
-  // })
-  // .get('/api', async (req, res) => {
-  //   data = await (new Promise(resolve => {
-  //     const cacheData = cache.get(keyData)
-  //     if (!cacheData) {
-  //       client.get(pm25API, async (data, response) => {
-  //         console.log('cached! ', cache.set(keyData, data))
-  //         resolve(data)
-  //       })
-  //     } else {
-  //       resolve(cacheData)
-  //     }
-  //   }))
+  .get('/history/:station', (req, res) => {
+    request(historyUrl + req.params.station, function (error, response, body) {
+      return res.status(200)
+      .type('text/html')
+      .send(body)
+    })
+  })
+  .get('/api', async (req, res) => {
+    data = await (new Promise(resolve => {
+      const cacheData = cache.get(keyData)
+      if (!cacheData) {
+        client.get(pm25API, async (data, response) => {
+          console.log('cached! ', cache.set(keyData, data))
+          resolve(data)
+        })
+      } else {
+        resolve(cacheData)
+      }
+    }))
 
-  //   let cordMaps = []
-  //   data['stations'].forEach(row => {
-  //     cordMaps[row.stationID] = {
-  //       latitude: row.lat,
-  //       longitude: row.long,
-  //     }
-  //   })
+    let cordMaps = []
+    data['stations'].forEach(row => {
+      cordMaps[row.stationID] = {
+        latitude: row.lat,
+        longitude: row.long,
+      }
+    })
     
-  //   let keysNearest = data['stations'].map(row => row.stationID)
+    let keysNearest = data['stations'].map(row => row.stationID)
 
-  //   if (req.query.lat && req.query.long) {
-  //     keysNearest = geolib.findNearest({latitude: req.query.lat, longitude: req.query.long}, cordMaps, 1, 5).map(row => row.key);
-  //   }
-  //   const resp = data['stations'].filter(row => keysNearest.includes(row.stationID)).map(row => ({
-  //     nameTH: row.nameTH,
-  //     nameEN: row.nameEN,
-  //     areaTH: row.areaTH,
-  //     areaEN: row.areaEN,
-  //     latitude: row.lat,
-  //     longitude: row.long,
-  //     updated: row.AQILast.date + ' ' + row.AQILast.time,
-  //     aqi: {
-  //       ...row.AQILast.AQI,
-  //       icon: icon.replace('%NUMBER%', row.AQILast.AQI.color_id)
-  //     },
-  //     historyUrl: req.hostname+'/history/'+row.stationID
-  //   }))
+    if (req.query.lat && req.query.long) {
+      keysNearest = geolib.findNearest({latitude: req.query.lat, longitude: req.query.long}, cordMaps, 1, 5).map(row => row.key);
+    }
+    const resp = data['stations'].filter(row => keysNearest.includes(row.stationID)).map(row => ({
+      nameTH: row.nameTH,
+      nameEN: row.nameEN,
+      areaTH: row.areaTH,
+      areaEN: row.areaEN,
+      latitude: row.lat,
+      longitude: row.long,
+      updated: row.AQILast.date + ' ' + row.AQILast.time,
+      aqi: {
+        ...row.AQILast.AQI,
+        icon: icon.replace('%NUMBER%', row.AQILast.AQI.color_id)
+      },
+      historyUrl: req.hostname+'/history/'+row.stationID
+    }))
 
-  //   return res.status(200)
-  //     .type('application/json')
-  //     .send(resp)
-  // })
+    return res.status(200)
+      .type('application/json')
+      .send(resp)
+  })
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
